a35eedaabc38962a46d4d27195e0c13d20591bd7
Tiffany <saxorynaffit@gmail.com>
2022-04-18 15:30:38 +0800

    Fixed Logo + Name CSS

--- frontend/src/LogReg/UI/Logo.js
+++ frontend/src/LogReg/UI/Logo.js
@@ -5,10 +5,10 @@ const Logo = () => {
   return (
     <React.Fragment>
         {/*Dunno whyy pa, pero this makes the logreg component go down(?), maybe dahil div?*/}
-        {/* <div className="logo-name-icon">
+        {<div className="logo-name-icon">
             <img src="https://bit.ly/3rmsS90" id="bulsu-logo" alt="BulSU Logo"/>
             <h3 id="site-name">BULACAN STATE UNIVERSITY</h3>
-        </div> */}
+        </div>}
       <img src="https://bit.ly/3EgPvkD" id="logreg-design" alt="App Logo" />
     </React.Fragment>
   );
