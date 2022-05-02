192041d881e0e491f1cd78cee0ecd57beb9f9292
gericofrancisco <gericofrancisco@gmail.com>
2022-05-02 10:13:47 +0800

    announcements + footer initial changes

--- frontend/src/Components/Main/Announcement/Single-Announcement.js
+++ frontend/src/Components/Main/Announcement/Single-Announcement.js
@@ -1,20 +1,25 @@
-import React from "react"
+import "./Announcement.css";
 
-const AnnouncementList = props =>{
-
-    return(
-        <ul className="announcement-list">
-            {props.AnList.map((item)=>{
-                return <li key={item.scrapeId}>
-                    <div>
-                        <h4>{item.scrapeTitle}</h4>
-                        <h3>{item.scrapeDate}</h3>
-                        <a href={item.scrapeURL} target="_blank" rel="noreferrer">title</a>
-                    </div>
-                </li>;
-            })}
-        </ul>
-    )
-}
+const AnnouncementList = (props) => {
+  return (
+    <>
+      <h1 className="announcement">Announcements</h1>
+      <hr className="hr" />
+      <br />
+      <div className="announcement-list">
+        {props.AnList.map((item) => {
+          return (
+            <div className="announcement-box" key={item.scrapeId}>
+              <a href={item.scrapeURL} target="_blank" rel="noreferrer">
+                <h3 className="announcement-title">{item.scrapeTitle}</h3>
+                <p className="announcement-date">{item.scrapeDate}</p>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default AnnouncementList;
--- frontend/src/Components/UI/Home_UI/Footer/Footer.js
+++ frontend/src/Components/UI/Home_UI/Footer/Footer.js
@@ -6,31 +6,36 @@ const Footer = () => {
       <p id="footer-uni">Bulacan State University</p>
       <p id="footer-label">CONTACT US</p>
       <div className="footer-contact">
-        <i
-          className="fa fa-facebook fb-icon"
-          style={{ fontSize: "1.25em" }}
-        ></i>
-        <a href="https://www.facebook.com/bulsuofficial">
-          <p id="footer-fb">BULACAN STATE UNIVERSITY</p>
-        </a>
-        <br />
-        <i
-          className="fas fa-phone phone-icon"
-          style={{ fontSize: "1.25em" }}
-        ></i>
-        <p id="footer-phone">919-7800</p>
-        <br />
-        <i
-          className="fas fa-map-marker-alt loc-icon"
-          style={{ fontSize: "1.25em" }}
-        ></i>
-        <p id="footer-loc">Guinhawa, City of Malolos, Bulacan</p>
-        <br />
-        <i
-          className="fas fa-envelope email-icon"
-          style={{ fontSize: "1.25em" }}
-        ></i>
-        <p id="footer-email">officeofthepresident@bulsu.edu.ph</p>
+        <span className="contact">
+          <i
+            className="fa fa-facebook fb-icon"
+            style={{ fontSize: "1.25em" }}
+          ></i>
+          <a className="contact-space" href="https://www.facebook.com/bulsuofficial">
+            <p id="footer-fb">BULACAN STATE UNIVERSITY</p>
+          </a>
+        </span>
+        <span className="contact">
+          <i
+            className="fas fa-phone phone-icon"
+            style={{ fontSize: "1.25em" }}
+          ></i>
+          <p className="contact-space" id="footer-phone">919-7800</p>
+        </span>
+        <span className="contact">
+          <i
+            className="fas fa-map-marker-alt loc-icon"
+            style={{ fontSize: "1.25em" }}
+          ></i>
+          <p className="contact-space" id="footer-loc">Guinhawa, City of Malolos, Bulacan</p>
+        </span>
+        <span className="contact">
+          <i
+            className="fas fa-envelope email-icon"
+            style={{ fontSize: "1.25em" }}
+          ></i>
+          <p className="contact-space" id="footer-email">officeofthepresident@bulsu.edu.ph</p>
+        </span>
       </div>
       <p id="copyright-bulsu">2022 Bulacan State University</p>
     </div>
